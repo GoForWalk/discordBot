bb587c5bce5c9bbd5dc9e622266346d5708d269a
GoForWalk <atcgsp912@gmail.com>
2021-08-17 15:37:26 +0900

    discordBot 1st

--- src/main/java/ExampleBot.java
+++ src/main/java/ExampleBot.java
@@ -1,24 +1,17 @@
-import net.dv8tion.jda.api.AccountType;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 import javax.security.auth.login.LoginException;
 
 public class ExampleBot {
-
-    private static final String BOT_TOKEN = "877022369754001438";
-
-    private static final String BOT_NAME = "partyBot";
-
-    public static void main(String[] args) throws LoginException, RateLimitedException, InterruptedException {
-
+    public static void main(String[] Args) throws LoginException, RateLimitedException, InterruptedException{
         JDABuilder builder = new JDABuilder((AccountType.BOT));
         builder.setToken(Args[0]);
         builder.setAutoReconnect(true);
         builder.setStatus(OnlineStatus.ONLINE);
-        JDA jda = builder.bu
+        JDA jda =  builder.buildBlocking();
     }
-
 }
