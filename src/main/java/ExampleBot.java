9d33c2ec734588cc954a323fb9ffb7429b077a94
GoForWalk <atcgsp912@gmail.com>
2021-08-18 14:50:07 +0900

    sec update

--- src/main/java/ExampleBot.java
+++ src/main/java/ExampleBot.java
@@ -7,7 +7,11 @@ import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import javax.security.auth.login.LoginException;
 
 public class ExampleBot {
+
+    private static String BOT_TOKEN = "ODc3MDIyMzY5NzU0MDAxNDM4.YRskuw.GbePJvE9F-GfwYgoCHQe-d7KDao";
+
     public static void main(String[] Args) throws LoginException, RateLimitedException, InterruptedException{
+
         JDABuilder builder = new JDABuilder((AccountType.BOT));
         builder.setToken(Args[0]);
         builder.setAutoReconnect(true);
